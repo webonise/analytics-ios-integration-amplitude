1ee45cbe675e4b3ea38942dbda31e4f31a5e69d8
SaketEywa <saket.pandhare@weboniselab.com>
2020-03-17 13:33:18 +0530

    Update Tests for DWEB-22

--- Example/Tests/Tests.m
+++ Example/Tests/Tests.m
@@ -183,7 +183,7 @@ describe(@"SEGAmplitudeIntegration", ^{
         it(@"sets groupId", ^{
             SEGGroupPayload *payload = [[SEGGroupPayload alloc] initWithGroupId:@"322" traits:@{} context:@{} integrations:@{}];
             [integration group:payload];
-            [verify(amplitude) setGroup:@"322" groupName:@"[Segment] Group"];
+            [verify(amplitude) setGroup:@"[Segment] Group" groupName:@"322"];
         });
 
         it(@"settings.groupTypeValue and settings.groupTypeTrait", ^{
@@ -205,7 +205,7 @@ describe(@"SEGAmplitudeIntegration", ^{
         it(@"sets group name with traits.name", ^{
             SEGGroupPayload *payload = [[SEGGroupPayload alloc] initWithGroupId:@"12342" traits:@{ @"name" : @"Segment" } context:@{} integrations:@{}];
             [integration group:payload];
-            [verify(amplitude) setGroup:@"12342" groupName:@"Segment"];
+            [verify(amplitude) setGroup:@"Segment" groupName:@"12342"];
         });
     });
 
